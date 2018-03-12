08f54231779f867013029861f8268cd0960484f9
uffe hellum <uffe.hellum@gmail.com>
2018-03-11 20:50:32 -0700

    dummy links

--- rgrjs/data/schema.js
+++ rgrjs/data/schema.js
@@ -1,35 +1,45 @@
 import {
-    GraphQLSchema,
-    GraphQLObjectType,
     GraphQLInt,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLSchema,
     GraphQLString
 } from 'graphql'
 
-let counter = 42
+let data = [
+    {_id: 'a1', title: 'title', url: 'http://url.com'},
+    {_id: 'a2', title: 'title 2', url: 'http://url2.com'},
+]
+
+let linkType = new GraphQLObjectType({
+    name: 'Link',
+    fields: () => ({
+        _id: { type: GraphQLString },
+        title: { type: GraphQLString },
+        url: { type: GraphQLString },
+    })
+})
 
 let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
-        fields: () =>({
-            counter: {
-                type: GraphQLInt,
-                resolve: () => counter,
-            },
-            message: {
-                type: GraphQLString,
-                resolve: () => 'hello graphql',
-            }
-        })
-    }),
-    mutation: new GraphQLObjectType({
-        name: 'Mutation',
         fields: () => ({
-            incrementCounter: {
-                type: GraphQLInt,
-                resolve: () => ++counter,
-            }
+            links: {
+                type: new GraphQLList(linkType),
+                resolve: () => data,
+            },
         })
     })
 })
+
+    // mutation: new GraphQLObjectType({
+    //     name: 'Mutation',
+    //     fields: () => ({
+    //         incrementCounter: {
+    //             type: GraphQLInt,
+    //             resolve: () => ++counter,
+    //         }
+    //     })
+    // })
 
 export default schema
