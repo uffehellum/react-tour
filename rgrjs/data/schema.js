262568da02256dd9439df1fc1f84fe9b30583ae4
uffe hellum <uffe.hellum@gmail.com>
2018-03-11 19:37:16 -0700

    graphql base

---
+++ rgrjs/data/schema.js
@@ -0,0 +1,25 @@
+import {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString
+} from 'graphql'
+
+let schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        fields: () =>({
+            counter: {
+                type: GraphQLInt,
+                resolve: () => 42,
+            },
+            message: {
+                type: GraphQLString,
+                resolve: () => 'hello graphql',
+            }
+        })
+    }),
+    // mutation: ...
+})
+
+export default schema
--- rgrjs/server.js
+++ rgrjs/server.js
@@ -1,9 +1,17 @@
 import express from 'express'
+import schema from './data/schema'
+import GraphQLHTTP from 'express-graphql'
 import {MongoClient} from 'mongodb'
 import {MONGO_URL} from './localconfig'
 
 let app = express()
 app.use(express.static('public'))
+
+app.use('/graphql', GraphQLHTTP({
+    schema,
+    graphiql: true,
+}))
+
 let db;
 MongoClient.connect(MONGO_URL, null, (err, client) => {
     if(err) throw err
